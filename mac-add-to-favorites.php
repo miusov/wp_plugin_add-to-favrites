24d8f3ffa69be895c58f0ccef1440789c01eadc8
miusov <miusov86@gmail.com>
2018-09-18 21:30:43 +0300

    create plugin

---
+++ mac-add-to-favorites.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Add to favorites
+Plugin URI: http://example.com
+Description: Добавление в избранное статей
+Version: 1.0
+Author: artem_miusov
+Author URI: https://github.com/miusov
+*/
+
