256d857f10ea672f492c507ec1a775270f22b6d1
miusov <miusov86@gmail.com>
2018-09-18 21:48:44 +0300

    add link in postPage

--- mac-add-to-favorites.php
+++ mac-add-to-favorites.php
@@ -8,3 +8,9 @@ Author: artem_miusov
 Author URI: https://github.com/miusov
 */
 
+add_filter('the_content','mac_add_to_favorites');
+
+function mac_add_to_favorites($content){
+    if ( !is_single() || !is_user_logged_in() ) return $content;
+    return '<p><a href="#" class="favorites-link">Add to favorites</a></p>' . $content;
+}
